489e783a30f77eaefafb912a696542d106a3dda3
BellWether <120770641+rituraj12797@users.noreply.github.com>
2023-12-28 10:58:33 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -4,7 +4,6 @@ var typeData = new Typed(".role", {
         
         "Frontend Developer",
         "MERN  developer",
-        "quant Enthusiast ",
         "Competitive-Coder",
     ],
     loop: true,
@@ -12,23 +11,7 @@ var typeData = new Typed(".role", {
     backSpeed: 80,
     backDelay: 1000,
 });
-const nameInput = document.getElementById('in1');
-const subjectInput = document.getElementById('in2');
-const emailInput = document.getElementById('in3');
-const messageInput = document.getElementById('in4');
-const sendButton = document.getElementById('send-button');
 
-// Function to clear the input values
-function clearInputs() {
-nameInput.value = '';
-subjectInput.value = '';
-emailInput.value = '';
-messageInput.value = '';
-}
 
-// Add an event listener to the button to trigger the clearing of inputs
-sendButton.addEventListener('click', () => {
-clearInputs();
-});
 
 
